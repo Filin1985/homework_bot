d241c24b825bedec1909d2929605e63d1a96b73c
Ихсанов Марат Азатович <M_Ihsanov@igirgi.su>
2022-08-22 17:19:11 +0300

    Реализовать функцию get_api_answer

--- homework.py
+++ homework.py
@@ -2,10 +2,18 @@ import os
 import requests
 import telegram
 import time
+from datetime import datetime
+import logging
+
 from dotenv import load_dotenv
+from http import HTTPStatus
 
 load_dotenv()
-
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='program.log',
+    format='%(asctime)s %(levelname)s %(message)s '
+)
 
 PRACTICUM_TOKEN = os.getenv('PR_TOKEN')
 TELEGRAM_TOKEN = os.getenv('BOT_TOKEN')
@@ -14,9 +22,6 @@ TELEGRAM_CHAT_ID = os.getenv('CHAT_ID')
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-
-
-
 
 HOMEWORK_STATUSES = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
@@ -32,14 +37,20 @@ def send_message(bot, message):
 def get_api_answer(current_timestamp):
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-    response = requests.get(ENDPOINT, headers=HEADERS, params=params)
-    print(response.text)
-
+    try:
+        response = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    except Exception as error:
+        logging.error(f'{datetime.now()} Запрос по адресу {ENDPOINT} не отработал')
+    if response.status != HTTPStatus.OK:
+        res_status = response.status
+        logging.error(f'{datetime.now()} Запрос по адресу {ENDPOINT} вернул результат {res_status}')
+        raise Exception(f'Запрос вернул статус {res_status}')
+    return response
 
 
 def check_response(response):
-
-    ...
+    if type(response) is not dict:
+        raise TypeError("Ответ получен в формате отличном от словаря")
 
 
 def parse_status(homework):
@@ -61,7 +72,6 @@ def check_tokens():
 
 def main():
     """Основная логика работы бота."""
-    get_api_answer(600)
     ...
 
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
@@ -71,7 +81,7 @@ def main():
 
     while True:
         try:
-            response = ...
+            response = get_api_answer(current_timestamp)
 
             ...
 
